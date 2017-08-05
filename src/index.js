62a044710148208c53bf06194455be1e0c88a807
lin-xi <linxi@iwaimai.baidu.com>
2017-08-05 13:59:03 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const classNameRegex = /\.(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)(?![^\{]*\})/gi
-const classInJSRegex = className => {
-  const re = new RegExp(
-    `('|")([-_a-zA-Z0-9-\\s]*)?` + className + `([-_a-zA-Z0-9-\\s]*)('|")`,
-    'g'
-  )
-  return re
-}
+const classInJSRegex = /class(?:Name)?\s*=\s*(?:'|")(.*?)(?:'|")/g
+// const classInJSRegex = className => {
+//   const re = new RegExp(
+//     `class(Name)?\s*=\s*('|")([-_a-zA-Z0-9-\\s]*)?` + className + `([-_a-zA-Z0-9-\\s]*)('|")`,
+//     'g'
+//   )
+//   return re
+// }
 const classInCSSRegex = className => {
   const re = RegExp(`\.` + className + `(\\s)?{[^\}]*\}`)
   return re
@@ -18,14 +19,16 @@ export class WebpackCssTreeShakingPlugin {
 
   apply (compiler) {
     compiler.plugin('emit', (compilation, callback) => {
+      // css文件
       const styleFiles = Object.keys(compilation.assets).filter(asset => {
         return /\.css$/.test(asset)
       })
-
+      // js文件
       const jsFiles = Object.keys(compilation.assets).filter(asset => {
         return /\.(js|jsx)$/.test(asset)
       })
 
+      // 获取所有的className数组
       const classNamesInStyles = styleFiles.reduce((acc, filename) => {
         const source = compilation.assets[filename].source()
         const match = source.match(classNameRegex).map(str => str.slice(1))
@@ -33,12 +36,14 @@ export class WebpackCssTreeShakingPlugin {
         return acc
       }, [])
 
+      // 获取所有js的代码字符串
       const jsContents = jsFiles.reduce((acc, filename) => {
         const contents = compilation.assets[filename].source()
         acc += contents
         return acc
       }, '')
 
+      // 获取所有js中没有出现的className
       const classesNotInJS = classNamesInStyles.filter(className => {
         if (
           this.options.ignore &&
@@ -46,9 +51,17 @@ export class WebpackCssTreeShakingPlugin {
         ) {
           return false
         }
-        return !classInJSRegex(className).test(jsContents)
+        let result = jsContents.match(classInJSRegex).some((str) => {
+          let mats = str.match(classInJSRegex)
+          if (mats && mats.length === 2) {
+            let cn = mats[1]
+            return cn === className || new RegExp(`${cn}\s*:`).test(className)
+          }
+        })
+        return !!result
       })
 
+      // 重复className删除
       const classesDeduped = classesNotInJS.reduce((acc, cur) => {
         if (acc.indexOf(cur) === -1) {
           acc.push(cur)
@@ -57,7 +70,7 @@ export class WebpackCssTreeShakingPlugin {
       }, [])
 
       if (this.options.showInfo) {
-        console.log(`Removing css classes:[${moduleName}]`, classesDeduped)
+        console.log(`Removing css classes:[${classesDeduped}]`)
       }
 
       if (!this.options.remove) {
