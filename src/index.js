8ccb2d0fd0f97c5a5edce0d33058e256bfc707b1
lin-xi <linxi@iwaimai.baidu.com>
2017-08-11 01:53:31 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,160 +1,45 @@
-const postcss = require('postcss')
-const cssshaking = require('postcss-tree-shaking')
-const fs = require('fs')
-const loaderUtils = require('loader-utils')
+var postcss = require('postcss')
+var treeShakingPlugin = require('./treeShakingPlugin')
 
-const NS = fs.realpathSync(__dirname)
-
-function cleanMap (map) {
-  if (map.sources) {
-    map.sources = map.sources.map((url) => url.split('://').pop())
-  }
-  return map
-}
-
-function filterModules (module) {
-  return module.loaders && module.loaders.reduce((r, l) => (r || l.loader.includes('/css-treeshaking-loader/')), false)
-}
-
-class CSSTreeshakingLoader {
-  constructor (compilation, query) {
-    this.options = {
-      plugins: false,
-      spinalCase: false,
-      ignore: [],
-      allowIds: false,
-      allowNonClassSelectors: false,
-      allowNonClassCombinators: false
-    }
-    if (query) {
-      Object.assign(this.options, query)
-    }
-    this.modules = []
-
-    const options = Object.assign({}, compilation.compiler.options)
-    options.output = Object.assign({}, options.output)
-    if (!this.options.plugin) {
-      options.plugins = []
-    }
-    delete options.devtool
-
-    const childCompiler = require('webpack/lib/webpack')(options)
-    // Increment recursion counter
-    childCompiler[NS] = compilation.compiler[NS] + 1
-
-    // Save our modules
-    childCompiler.plugin('compilation', (comp) => {
-      comp.plugin('after-optimize-modules', (modules) => {
-        this.modules = modules.filter(filterModules)
-      })
-    })
-    childCompiler.plugin('after-compile', (comp, callback) => {
-      // Remove all chunk assets
-      comp.chunks.forEach(function (chunk) {
-        chunk.files.forEach(function (file) {
-          delete comp.assets[file]
-        })
-      })
-      callback()
-    })
-    this.childCompiler = childCompiler
+export default class CSSTreeShakePlugin {
+  constructor (options) {
+    this.options = Object.assign({}, {remove: true, showInfo: true}, options)
   }
 
-  doChildCompilation () {
-    if (!this.compilePromise) {
-      this.compilePromise = new Promise((resolve, reject) => {
-        this.childCompiler.run((err) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve()
+  apply (compiler) {
+    compiler.plugin('emit', (compilation, callback) => {
+		  const styleFiles = Object.keys(compilation.assets).filter(asset => {
+			  return /\.css$/.test(asset)
+		  })
+
+		  const jsFiles = Object.keys(compilation.assets).filter(asset => {
+			  return /\.(js|jsx)$/.test(asset)
+  })
+
+      const jsContents = jsFiles.reduce((acc, filename) => {
+			  const contents = compilation.assets[filename].source()
+			  acc += contents
+			  return acc
+      }, '')
+
+      let tasks = []
+      styleFiles.forEach((filename) => {
+        const source = compilation.assets[filename].source()
+        let listOpts = {
+          include: 'ids',
+          source: jsContents
+        }
+        tasks.push(postcss(treeShakingPlugin(listOpts, (src) => {
+          compilation.assets[filename] = {
+            source: () => src,
+            size: () => src.length
           }
-        })
+        })).process(source))
       })
-    }
-    return this.compilePromise
+
+      Promise.all(tasks).then(() => {
+        callback()
+      })
+	  })
   }
-
-  filterCSS (loaderContext, source, sourceMap) {
-    return new Promise((resolve, reject) => {
-      this.doChildCompilation().then(() => {
-        const module = this.getModule(loaderContext.request)
-
-        if (module.error) {
-          return reject(module.error)
-        }
-
-        if (module.usedExports === true || module.usedExports === false || module.usedExports.indexOf('default') !== -1) {
-          return resolve({ source: source, map: sourceMap })
-        }
-
-        let usedSelectors = module.usedExports.filter((selector) => selector !== '$css')
-
-        if (this.options.spinalCase) {
-          usedSelectors = usedSelectors.concat(
-                        usedSelectors.map((selector) => selector.replace(/([a-z0-9])([A-Z])/g, (match, p1, p2) => p1 + '-' + p2.toLowerCase()))
-                    )
-        }
-
-        postcss([
-          cssshaking({
-            used: usedSelectors,
-            ignore: this.options.ignore,
-            allowIds: this.options.allowIds,
-            allowNonClassSelectors: this.options.allowNonClassSelectors,
-            allowNonClassCombinators: this.options.allowNonClassCombinators
-          })
-        ]).process(source, {
-          from: loaderUtils.getRemainingRequest(loaderContext),
-          to: loaderUtils.getCurrentRequest(loaderContext),
-          map: {
-            prev: sourceMap,
-            sourcesContent: true,
-            inline: false,
-            annotation: false
-          }
-        }).then((result) => {
-          resolve({ source: result.css, map: cleanMap(result.map.toJSON())})
-        }).catch((error) => {
-          reject(error)
-        })
-      }).catch((err) => reject(err))
-    })
-  }
-
-  getModule (request) {
-    return this.modules.filter((module) => module.request.includes(request))[0]
-  }
-}
-
-function loader (source, map) {
-  if (this.cacheable) this.cacheable()
-
-  const query = loaderUtils.parseQuery(this.query)
-
-  if (typeof query.recursion === 'undefined') {
-    query.recursion = 1
-  }
-
-  if (typeof this._compiler[NS] === 'undefined') {
-    this._compiler[NS] = 0
-  }
-    // Don't apply transform to final recursion
-  if (this._compiler[NS] === query.recursion) {
-    this.callback(null, source, map)
-  } else {
-    const callback = this.async()
-
-        // Save loader instance on the compilation
-    const loaderId = 'DeadCSSLoader' + (typeof this.query === 'string' ? this.query : '?' + JSON.stringify(this.query))
-    this._compilation[loaderId] = this._compilation[loaderId] || new CSSTreeshakingLoader(this._compilation, query)
-
-    this._compilation[loaderId].filterCSS(this, source, map).then((result) => {
-      callback(null, result.source, result.map)
-    }).catch((err) => callback(err))
-  }
-}
-
-loader.extract = function (options) {
-  return { loader: require.resolve('./extract'), query: options }
 }
---
+++ src/treeShakingPlugin.js
@@ -0,0 +1,131 @@
+var postcss = require('postcss')
+var selectorParser = require('postcss-selector-parser')
+var _ = require('lodash')
+var processIncludes = require('./processIncludes')
+
+function selectorSort (selector) {
+  var lowerNoPseudo = selector.split(':')[0].toLowerCase()
+  return (/^[#\.\[]/.test(selector)) ? lowerNoPseudo.substr(1) : lowerNoPseudo
+}
+
+const classInJSRegex = (className) => {
+  const re = new RegExp(`('|")([-_a-zA-Z0-9-\\s]*)?` + className + `([-_a-zA-Z0-9-\\s]*)('|")`, 'g')
+  return re
+}
+
+/**
+ * The postcss plugin at the heart of everything.
+ *
+ * @param {object|function} [options] - If this is a function it's interpreted as the callback
+ * @param {function} [callback] - Callback, which will receive the list of selectors as its argument
+ */
+module.exports = postcss.plugin('list-selectors', function (options, callback) {
+  var opts, cb
+  if (_.isFunction(options)) {
+    opts = {}
+    cb = options
+  } else {
+    opts = options || {}
+    cb = callback || _.noop
+  }
+
+  return function (cssRoot, postcssResult) {
+    var selectorObj = {}
+
+    // Run through all the rules and accumulate a list of selectors
+    // parsed out by PostCSS
+    var accumulatedSelectors = []
+    cssRoot.walkRules(function (rule) {
+      // Ignore keyframes, which can log e.g. 10%, 20% as selectors
+      if (rule.parent.type === 'atrule' && /keyframes/.test(rule.parent.name)) return
+
+      rule.selectors.forEach(function (selector) {
+        accumulatedSelectors.push(selector)
+      })
+    })
+
+    // If no selectors were found, results are an empty object
+    if (_.isEmpty(accumulatedSelectors)) {
+      postcssResult.warn(
+        'Failed to find any selectors at all in the source files you provided. ' +
+        'You are going to get an empty selector list.'
+      )
+      cb({})
+      return
+    }
+
+    // Add sorted, unique selectors to results
+    var sortedUniqueSelectors = _.chain(accumulatedSelectors)
+      .uniq()
+      .sortBy(selectorSort)
+      .value()
+
+    selectorObj.selectors = sortedUniqueSelectors
+
+    selectorObj.simpleSelectors = {
+      all: [],
+      ids: [],
+      classes: [],
+      attributes: [],
+      types: []
+    }
+
+    // Add sorted, unique *simple* selectors to results
+    sortedUniqueSelectors.forEach(function (selector) {
+      selectorParser(function (selectors) {
+        selectors.eachInside(function (node) {
+          // Various nodes the selector-parser loops through should be ignored
+          if (_.includes(['selector', 'comment', 'combinator', 'pseudo'], node.type)) return
+          // Arguments of `:nth-*` pseudo-classes should be ignored, as they
+          // are integers, an+b expressions, or "odd" and "even"
+          if (node.parent.parent.value && node.parent.parent.value.substr(0, 5) === ':nth-') return
+          selectorObj.simpleSelectors.all.push(node.toString())
+          switch (node.type) {
+            case 'id':
+              selectorObj.simpleSelectors.ids.push(node.toString())
+              break
+            case 'class':
+              selectorObj.simpleSelectors.classes.push(node.toString())
+              break
+            case 'attribute':
+              selectorObj.simpleSelectors.attributes.push(node.toString())
+              break
+            case 'tag':
+              selectorObj.simpleSelectors.types.push(node.toString())
+              break
+            default:
+          }
+        })
+      }).process(selector)
+    })
+
+    _.forOwn(selectorObj.simpleSelectors, function (selectorList, key) {
+      selectorObj.simpleSelectors[key] = _.chain(selectorList)
+        .flatten()
+        .uniq()
+        .sortBy(selectorSort)
+        .value()
+    })
+
+    // // Refine the results according to any `include` options passed
+    selectorObj = processIncludes(selectorObj, opts.include, postcssResult)
+
+    const classesNotInJS = selectorObj.filter((className) => {
+      if (opts.ignore && opts.ignore.indexOf(className) !== -1) {
+        return false
+      }
+      return !classInJSRegex(className).test(opts.source)
+    })
+
+    cssRoot.walkRules(function (rule) {
+      console.log(rule)
+      // Ignore keyframes, which can log e.g. 10%, 20% as selectors
+      if (rule.type === 'class' && classesNotInJS.indexof(rule.name) !== -1) {
+        rule.remove()
+      }
+    })
+
+    // Call the callback as promised, passing the selectorObj as an argument
+    cb(selectorObj)
+  }
+})
